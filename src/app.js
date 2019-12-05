6fcaf024d395c481b7e0eef12e863a1af46d0228
Richard <info@monkemedia.co.uk>
2019-12-05 16:26:07 +0000

    Added uploadcare
    and added mongo to route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoRouters = require('./routers/mongo/index.js')
-// const uploadcareRouters = require('./routers/uploadcare/index.js')
+const uploadcareRouters = require('./routers/uploadcare/index.js')
 const oauth = require('./routers/mongo/oauth.js')
 const bodyParser = require('body-parser')
 
@@ -14,8 +14,8 @@ app.use(cors())
 
 app.use(bodyParser.json())
 app.use('/oauth/', oauth)
-app.use('/v1/', mongoRouters)
-// app.use('/v1/', uploadcareRouters)
+app.use('/v1/mongo', mongoRouters)
+app.use('/v1/images', uploadcareRouters)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
---
+++ src/models/uploadcare/Image.js
@@ -0,0 +1,8 @@
+// const uploadcare = require('../lib/main')(process.env.UPLOADCARE_PUBLIC_KEY, process.env.UPLOADCARE_PRIVATE_KEY)
+// const fs = require('fs')
+
+const upload = () => {
+  console.log('UPLOAD')
+}
+
+exports.upload = upload
