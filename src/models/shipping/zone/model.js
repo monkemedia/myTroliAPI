6410e2cc35ec68ea1cc16317de75ca89d06421a8
Richard <info@monkemedia.co.uk>
2020-06-17 20:35:37 +0100

    Shipping zones created

---
+++ src/models/shipping/zone/model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const shippingZoneSchema = require('./schema')
+
+// Get zones
+shippingZoneSchema.statics.findZones = async () => {
+  const zones = await ShippingZone.find({})
+  return zones
+}
+
+// Get zone
+shippingZoneSchema.statics.findZone = async (zoneId) => {
+  const zone = await ShippingZone.findOne({ _id: zoneId })
+  return zone
+}
+
+// Update zone
+shippingZoneSchema.statics.updateZone = async (zoneId, zoneDetails) => {
+  const zone = await ShippingZone.updateOne({ _id: zoneId }, zoneDetails)
+  return zone
+}
+
+// Delete zone
+shippingZoneSchema.statics.deleteZone = async (zoneId) => {
+  const zone = await ShippingZone.deleteOne({ _id: zoneId })
+  return zone
+}
+
+const ShippingZone = mongoose.model('ShippingZone', shippingZoneSchema)
+
+module.exports = ShippingZone
