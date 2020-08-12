691114e2976f19b3849af1a7f40aac1eaf66860b
Richard <info@monkemedia.co.uk>
2020-08-12 09:11:44 +0100

    Created product filtering endpoints

---
+++ src/controller/productFiltering.js
@@ -0,0 +1,66 @@
+const ProductFiltering = require('../models/productFiltering')
+
+const updateFacetSettings = async (req, res) => {
+  const data = req.body
+  const { type } = data
+
+  if (!type) {
+    return res.status(401).send({
+      message: 'Type is required'
+    })
+  }
+
+  if (type && type !== 'product-filtering') {
+    return res.status(401).send({
+      message: 'Correct type is required'
+    })
+  }
+
+  try {
+    await ProductFiltering.updateFacetSettings(data)
+    const facets = await ProductFiltering.findFacetSettings()
+
+    res.status(200).send(facets)
+  } catch (err) {
+    res.status(400).send(err)
+  }
+}
+
+const getFacetSettings = async (req, res) => {
+  try {
+    const facets = await ProductFiltering.findFacetSettings()
+
+    res.status(200).send(facets)
+  } catch (err) {
+    res.status(400).send(err)
+  }
+}
+
+const getFacets = async (req, res) => {
+  try {
+    const facets = await ProductFiltering.findFacets()
+
+    res.status(200).send(facets)
+  } catch (err) {
+    res.status(400).send(err)
+  }
+}
+
+// const getFilterByName = async (req, res) => {
+//   const facetName = req.params.facetName
+
+//   try {
+//     const facets = await ProductFilter.findFilterByName(facetName)
+
+//     res.status(200).send(facets)
+//   } catch (err) {
+//     res.status(400).send(err)
+//   }
+// }
+
+module.exports = {
+  updateFacetSettings,
+  getFacetSettings,
+  getFacets
+  // getFilterByName
+}
---
+++ src/models/productFiltering/schema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const productFilterSchema = new Schema({
+  type: {
+    type: String
+  },
+  facets: [{
+    name: {
+      type: String
+    },
+    slug: {
+      type: String
+    },
+    sort_order: {
+      type: Number
+    },
+    visible: {
+      type: Boolean
+    }
+  }],
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date
+  }
+  
+}, { versionKey: false })
+
+module.exports = productFilterSchema
