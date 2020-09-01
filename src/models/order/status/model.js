f75d99d7283db6fe4aae6f61aa9d172a97d767d4
Richard <info@monkemedia.co.uk>
2020-09-01 18:08:33 +0100

    brought back order status

--- src/models/order/status/model.js
+++ src/models/order/status/model.js
@@ -8,21 +8,6 @@ OrderStatusSchema.statics.findOrderStatuses = async () => {
   return orderStatuses
 }
 
-// Update order status
-OrderStatusSchema.statics.updateOrderStatus = async (statusOrderId, orderStatusDetails) => {
-  const orderStatus = await OrderStatus.updateOne({ status_id: statusOrderId }, {
-    ...orderStatusDetails,
-    updated_at: Date.now()
-  })
-  return orderStatus
-}
-
-// Delete order status
-OrderStatusSchema.statics.deleteOrderStatus = async (orderStatusId) => {
-  const orderStatus = await OrderStatus.deleteOne({ status_id: orderStatusId })
-  return orderStatus
-}
-
 const OrderStatus = mongoose.model('OrderStatus', OrderStatusSchema)
 
 module.exports = OrderStatus
