65feaa194e77eda87a54452e51a72c2e0b58f6d1
Richard <info@monkemedia.co.uk>
2020-09-23 09:37:57 +0100

    added united kingdom to top of country list

--- src/models/country/model.js
+++ src/models/country/model.js
@@ -3,8 +3,16 @@ const CountrySchema = require('./schema')
 
 // Get countries
 CountrySchema.statics.findCountries = async () => {
-  const countries = await Country.find({})
+  const countries = await Country
+    .find({})
+    .sort({ country: 1 })
+    
+  countries.some((item, idx) => item.country_iso === 'GB' && countries.unshift(countries.splice(idx,1)[0]))
 
+  countries.splice(1, 0, {
+    country: '-----------------------------'
+  })
+  
   return countries
 }
 
