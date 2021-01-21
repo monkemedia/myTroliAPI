a2b3f3aec78329f71b43c215e9a157a569f0bb62
Richard <info@monkemedia.co.uk>
2021-01-21 16:10:10 +0000

    cors

--- src/app.js
+++ src/app.js
@@ -10,12 +10,7 @@ const app = express()
 
 require('./db/masterDb.js')
 
-const corsOptions = {
-  origin: process.env.WEB_ADDRESS,
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/oauth/', oauth)
