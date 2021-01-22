65d9f75c17132f99d9d387fa5373680ce22f60c1
Richard <info@monkemedia.co.uk>
2021-01-22 10:21:08 +0000

    cors

--- src/routers/oauth.js
+++ src/routers/oauth.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const router = express.Router()
 const {
   refreshToken,
@@ -11,7 +10,7 @@ const {
 // Get access token from refresh token
 router.post('/refresh-token', (req, res) => refreshToken(req, res))
 // Get access token when user logins in
-router.post('/access-token', cors(option), (req, res) => accessToken(req, res))
+router.post('/access-token', (req, res) => accessToken(req, res))
 // Send reset token to merchants email
 router.post('/reset-token', (req, res) => resetToken(req, res))
 // Reset password
